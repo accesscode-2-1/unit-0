43939a3f4f8f1508b781dbddcce9edae3e09dac2
John Rodriguez <john.rodriguez@gmail.com>
2015-03-21 20:15:36 -0400

    add behavior in BankAccount for withdraw & deposit

--- in-class exercise solutions/2015-03-21/BankAccount/src/BankAccount.java
+++ in-class exercise solutions/2015-03-21/BankAccount/src/BankAccount.java
@@ -9,9 +9,23 @@ public class BankAccount {
         this.balance = balance;
     }
 
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
     public static void main(String[] args) {
         BankAccount a1 = new BankAccount(100);
         BankAccount a2 = new BankAccount();
+
+        System.out.println(a1.balance);
+        System.out.println(a2.balance);
+
+        a1.withdraw(5);
+        a2.deposit(200);
 
         System.out.println(a1.balance);
         System.out.println(a2.balance);
