17408e590804d69a227ec13a8cc4138f2de34a5e
John Rodriguez <john.rodriguez@gmail.com>
2015-03-21 20:11:15 -0400

    add main in BankAccount for testing purposes

--- in-class exercise solutions/2015-03-21/BankAccount/src/BankAccount.java
+++ in-class exercise solutions/2015-03-21/BankAccount/src/BankAccount.java
@@ -1,9 +1,19 @@
-/**
- * Created with IntelliJ IDEA.
- * User: jrodriguez
- * Date: 3/21/15
- * Time: 8:08 PM
- * To change this template use File | Settings | File Templates.
- */
 public class BankAccount {
+    private int balance;
+
+    public BankAccount() {
+        this.balance = 0;
+    }
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    public static void main(String[] args) {
+        BankAccount a1 = new BankAccount(100);
+        BankAccount a2 = new BankAccount();
+
+        System.out.println(a1.balance);
+        System.out.println(a2.balance);
+    }
 }
