4e34fa2e82699acf6a6f2dc8129c6b80c1142c6b
John Rodriguez <john.rodriguez@gmail.com>
2015-03-21 20:09:08 -0400

    create BankAccount class, add constructors

---
+++ in-class exercise solutions/2015-03-21/BankAccount/src/BankAccount.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jrodriguez
+ * Date: 3/21/15
+ * Time: 8:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BankAccount {
+}
