d4893081076f655a703cef3b88f166afcb04be3f
John Rodriguez <john.rodriguez@gmail.com>
2015-03-21 20:22:53 -0400

    create Main for testing interaction between BankAccount and Patron

---
+++ in-class exercise solutions/2015-03-21/BankAccount/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //Scenario 1: test account/patron interaction
+
+        //create account
+        BankAccount c4qAccount = new BankAccount(1000);
+
+        // create patron, associating account with patron
+        Patron john = new Patron("John", 0, c4qAccount);
+
+        // do something (behavior)
+        john.getMoneyFromAccount(10);
+
+        // verify state
+        john.printPocket();
+        c4qAccount.printBalance();
+    }
+}
