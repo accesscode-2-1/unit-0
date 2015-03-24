9fa2b022b8650501760821d3300a8bdd7dd98647
gwg <ggundersen@gmail.com>
2015-03-24 00:41:29 -0400

    initial commit

---
+++ in-class exercise solutions/BlueBook/Car.java
@@ -0,0 +1,34 @@
+/**
+ * Access Code 2.1
+ *
+ * Author  Gregory Gundersen
+ * Date    March 2015
+ *
+ * This class represents a car.
+ */
+
+public class Car {
+
+    String make;
+    String model;
+    int year;
+    double price;
+
+    public Car(String make, String model, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getMake() { return this.make; }
+
+    public String getModel() { return this.model; }
+
+    public int getYear() { return this.year; }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+}
