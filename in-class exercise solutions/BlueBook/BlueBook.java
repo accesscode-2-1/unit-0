4d8f66532f3397e8a9f2fa814807647d6b6c895b
gwg <ggundersen@gmail.com>
2015-03-24 00:50:41 -0400

    Stubbed out directions for exercise

---
+++ in-class exercise solutions/BlueBook/BlueBook.java
@@ -0,0 +1,31 @@
+/**
+ * Access Code 2.1
+ *
+ * Author  <YOUR NAME>
+ * Date    March 2015
+ *
+ * This class asks the user which car they have and prints some facts about it.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BlueBook {
+
+    public static void main(String[] args) {
+        // 1. Create a Scanner for user input.
+
+        // 2. Create an ArrayList that holds type Car.
+
+        // 3. Create a few new Car objects and add them to the ArrayList.
+
+        // 4. Prompt the user for the car's make, model, and year, and store the inputs in variables.
+
+        // 5. Iterate over your ArrayList of cars and check for a Car instance that has the same...
+        //    ...make
+        //    ...model
+        //    ...year
+
+        // 6. Print the price of that car.
+    }
+}
