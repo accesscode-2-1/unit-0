05b0561428956e14b964befe78c91c7b9eea7308
Alex Samuel <alex@alexsamuel.net>
2015-03-29 11:09:53 -0400

    Boxed types demo.

--- projects/2015-03-29/src/nyc/c4q/ac21/MoonsAndPlanets.java
+++ projects/2015-03-29/src/nyc/c4q/ac21/MoonsAndPlanets.java
@@ -1,4 +1,27 @@
 package nyc.c4q.ac21;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class MoonsAndPlanets {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> numberOfMoons = new HashMap<String, Integer>();
+
+        numberOfMoons.put("Earth", 14);
+        numberOfMoons.put("Mercury", 0);
+        numberOfMoons.put("Mars", 2);
+        numberOfMoons.put("Venus", 0);
+
+        Scanner scanner = new Scanner(System.in);
+        String planet;
+        System.out.print("name a planet: ");
+        planet = scanner.next();
+        System.out.print("how many moons does " + planet + " have? ");
+        int numMoons = scanner.nextInt();
+        numberOfMoons.put(planet, numMoons);
+
+        System.out.println(numberOfMoons.get("Texasa"));
+    }
+
 }
