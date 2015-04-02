4e40f35fc6b564a5eaca3fa38ff033633be081ef
gwg <ggundersen@gmail.com>
2015-04-02 09:28:23 -0400

    Added printlns

--- in-class exercise solutions/SocialNetwork.java
+++ in-class exercise solutions/SocialNetwork.java
@@ -5,36 +5,42 @@ public class SocialNetwork {
     public static void main(String[] args) {
         HashSet<String> yours = new HashSet();
         yours.add("Yelena");
-        yours.add("Jamal");
+        yours.add("Yasiko");
         yours.add("Eve");
         HashSet<String> mine = new HashSet();
-        mine.add("Sebastian");
+        mine.add("Maddie");
         mine.add("Mala");
         mine.add("Eve");
-        
-        // No method should mutate the input sets. This means you need to create and return a *new* set!
-        // This will let you combine methods. For example:
-        // yourFriendsAreMine( allFriends(yours, mine), mine); // true
+
+        // You should be able to run all of these in a row:
+        //System.out.println( allFriends(yours, mine) );
+        //System.out.println( mutualFriends(yours, mine) );
+        //System.out.println( yourFriends(yours, mine) );
+        //System.out.println( myFriends(yours, mine) );
+        //System.out.println( exclusiveFriends(yours, mine) );
+        //System.out.println( yourFriendsAreMine(yours, mine) );
+        //System.out.println( youHaveFriends(yours, mine) );
+        //System.out.println( matchmaker(yours, mine) );
     }
 
     /*public static HashSet<String> allFriends(HashSet<String> yours, HashSet<String> mine) {
-        // Yelena, Jamal, Eve, Sebastian, Mala
+        // Yelena, Yasiko, Eve, Maddie, Mala
     }*/
 
     /*public static HashSet<String> mutualFriends(HashSet<String> yours, HashSet<String> mine) {
         // Eve
     }*/
 
-    /*public static HashSet<String> justYourFriends(HashSet<String> yours, HashSet<String> mine) {
-        // Yelena, Jamal
+    /*public static HashSet<String> yourFriends(HashSet<String> yours, HashSet<String> mine) {
+        // Yelena, Yasiko
     }*/
 
-    /*public static HashSet<String> justMyFriends(HashSet<String> yours, HashSet<String> mine) {
-        // Sebastian, Mala
+    /*public static HashSet<String> myFriends(HashSet<String> yours, HashSet<String> mine) {
+        // Maddie, Mala
     }*/
 
     /*public static HashSet<String> exclusiveFriends(HashSet<String> yours, HashSet<String> mine) {
-        // Yelena, Jamal, Sebastian, Mala
+        // Yelena, Yasiko, Maddie, Mala
     }*/
 
     /*public static boolean yourFriendsAreMine(HashSet<String> yours, HashSet<String> mine) {
@@ -46,17 +52,15 @@ public class SocialNetwork {
     }*/
 
     /*public static HashSet<HashSet<String>> matchmaker(HashSet<String> yours, HashSet<String> mine) {
-        // should return these sets:
+        [Maddie, Yasiko]
+        [Yasiko, Mala]
+        [Yasiko, Eve]
 
-        // [Sebastian, Jamal]
-        // [Jamal, Mala]
-        // [Jamal, Eve]
+        [Maddie, Yelena]
+        [Yelena, Mala]
+        [Yelena, Eve]
 
-        // [Sebastian, Yelena]
-        // [Yelena, Mala]
-        // [Yelena, Eve]
-
-        // [Sebastian, Eve]
-        // [Mala, Eve]
+        [Maddie, Eve]
+        [Mala, Eve]
     }*/
 }
